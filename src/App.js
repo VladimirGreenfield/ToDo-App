558c15179fe239b7e336a33357de5b14b43ec869
vladimir greenfield <vladdygreenfield@gmail.com>
2022-01-06 23:54:44 -0500

    created hooks, created onChange event

--- src/App.js
+++ src/App.js
@@ -6,20 +6,44 @@ import React, { useState} from 'react';
 
 function App() {
 
-  const [toDoList, setToDoList] = useState([]);
+  //Set up To-Do list
+  const [toDoList, setToDoList] = useState([
+    {text: "Work on my resume"},
+    {text: "Apply for jobs"},
+    {text: "Clean the house"}
+  ]);
 
+  //Set up Input value
   const [value, setValue] = useState('');
+
+  //Add To-Do
+
+
+
+  //Delete To-Do
+
+  //Handle the submit of the form
 
 
 
   return (
     <div className="App">
       <h1>My To-Do List</h1>
-      <div className="toDoInput">
-        <form>
-          <input></input>
-        </form>
-        <button>Add To-Do</button>
+      <div className="container">
+        {
+          toDoList.map((todo, index) => (
+            <div key={index}>
+              <span>{todo.text}</span>
+              <button>Delete</button>
+            </div>
+          ))
+        }
+        <div className="formInput">  
+          <form placeholder="Type To-Do Here" value={value} onChange={keyboardStroke => setValue(keyboardStroke.target.value)}>
+            <input></input>
+          </form>
+          <button>Add To-Do</button>
+        </div>  
       </div>
     </div>
   );
